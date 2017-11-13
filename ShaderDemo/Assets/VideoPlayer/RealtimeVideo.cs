eae2293052b5606e21e1ac5a893ad2c4c37daf8b
ytbb0530 <31501967+ytbb0530@users.noreply.github.com>
2017-11-13 17:55:55 +0800

    实时计算video

---
+++ ShaderDemo/Assets/VideoPlayer/RealtimeVideo.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+
+public class RealtimeVideo : MonoBehaviour 
+{
+	public Material mat;
+	public Text textWH;
+	public Slider slider;
+	public RawImage image;
+
+	private VideoPlayer vplayer;
+	private int mx;
+	private int my;
+
+
+	void Start()
+	{
+		slider.minValue = .1f;
+		slider.maxValue = .4f;
+		slider.value = .25f;
+		vplayer = GetComponent<VideoPlayer> ();
+		valueChange ();
+	}
+
+	public void createMesh () 
+	{
+		slider.transform.parent.gameObject.SetActive (false);
+		RenderTexture renderTex = new RenderTexture (mx, my, 1);
+		vplayer.targetTexture = renderTex;
+		image.texture = renderTex;
+
+		int w = renderTex.width;
+		int h = renderTex.height;
+
+		List<Vector3> vertices = new List<Vector3>();
+		List<int> triangles = new List<int> ();
+		List<Vector2> uv_0 = new List<Vector2> ();
+		List<Vector2> uv_1 = new List<Vector2> ();
+
+		for (int x = 0; x < w - 1; x++) {
+			for (int y = 0; y < h - 1; y++) {
+
+				Vector3 p0 = new Vector3 (x, y, 0);
+				Vector3 p1 = new Vector3 (x, y+1, 0);
+				Vector3 p2 = new Vector3 (x+1, y+1, 0);
+				Vector3 p3 = new Vector3 (x+1, y, 0);
+
+				vertices.Add (p0 + new Vector3(-(w-1)/2f, -(h-1)/2f, 0));
+				vertices.Add (p1 + new Vector3(-(w-1)/2f, -(h-1)/2f, 0));
+				vertices.Add (p2 + new Vector3(-(w-1)/2f, -(h-1)/2f, 0));
+				vertices.Add (p3 + new Vector3(-(w-1)/2f, -(h-1)/2f, 0));
+
+				triangles.Add (vertices.Count - 4);
+				triangles.Add (vertices.Count - 3);
+				triangles.Add (vertices.Count - 2);
+				triangles.Add (vertices.Count - 1);
+
+				Vector2 uv = new Vector2 (((p0.x+p3.x)/2)/w, ((p0.y+p1.y)/2)/h);
+
+				uv_0.Add (uv);
+				uv_0.Add (uv);
+				uv_0.Add (uv);
+				uv_0.Add (uv);
+
+				uv_1.Add (new Vector2(0, 0));
+				uv_1.Add (new Vector2(0, 1));
+				uv_1.Add (new Vector2(1, 1));
+				uv_1.Add (new Vector2(1, 0));
+			}
+		}
+
+		Mesh mesh = new Mesh ();
+		mesh.Clear ();
+		mesh.SetVertices(vertices);
+		mesh.SetIndices(triangles.ToArray(), MeshTopology.Quads, 0 );
+		mesh.SetUVs (0, uv_0);
+		mesh.SetUVs (1, uv_1);
+
+		GameObject gc = new GameObject ();
+
+		MeshFilter meshFilter = gc.AddComponent<MeshFilter>();
+		meshFilter.sharedMesh = mesh;
+
+		MeshRenderer meshRender = gc.AddComponent<MeshRenderer> ();
+		meshRender.material = mat;
+		meshRender.material.SetTexture ("_MainTex", renderTex);
+
+		float camSize = h / 2;
+		if (w * 1f / h > Camera.main.aspect) {
+			camSize *= (w * 1f / h) - Camera.main.aspect + 1;
+		}
+		Camera.main.orthographicSize = (int)camSize + 4;
+
+		vplayer.Play ();
+	}
+
+	public void PR()
+	{
+		if (vplayer.isPlaying) {
+			vplayer.Pause ();
+		} else {
+			vplayer.Play ();
+		}
+	}
+
+	public void valueChange () 
+	{
+		mx = (int)(426 * slider.value);
+		my = (int)(240 * slider.value);
+		textWH.text = mx + "/" + my;
+	}
+}
---
+++ ShaderDemo/Assets/VideoPlayer/RealtimeVideo.shader
@@ -0,0 +1,100 @@
+﻿Shader "Unlit/RealtimeVideo"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+		_Font0 ("font_0", 2D) = "white" {}
+		_Font1 ("font_1", 2D) = "white" {}
+		_Font2 ("font_2", 2D) = "white" {}
+		_Font3 ("font_3", 2D) = "white" {}
+		_Font4 ("font_4", 2D) = "white" {}
+		_Font5 ("font_5", 2D) = "white" {}
+		_Font6 ("font_6", 2D) = "white" {}
+		_Font7 ("font_7", 2D) = "white" {}
+	}
+	SubShader
+	{
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			struct a2v
+			{
+				float4 vertex : POSITION;
+				float2 uv1 : TEXCOORD0;
+				float2 uv2 : TEXCOORD1;
+			};
+
+			struct v2f
+			{
+				float2 uv1 : TEXCOORD0;
+				float2 uv2 : TEXCOORD1;
+				float4 vertex : SV_POSITION;
+			};
+
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			sampler2D _Font0;
+			float4 _Font0_ST;
+			sampler2D _Font1;
+			float4 _Font1_ST;
+			sampler2D _Font2;
+			float4 _Font2_ST;
+			sampler2D _Font3;
+			float4 _Font3_ST;
+			sampler2D _Font4;
+			float4 _Font4_ST;
+			sampler2D _Font5;
+			float4 _Font5_ST;
+			sampler2D _Font6;
+			float4 _Font6_ST;
+			sampler2D _Font7;
+			float4 _Font7_ST;
+			
+			v2f vert (a2v v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv1 = TRANSFORM_TEX(v.uv1, _MainTex);
+				o.uv2 = TRANSFORM_TEX(v.uv2, _Font1);
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				fixed4 col = tex2D(_MainTex, i.uv1);
+
+				fixed4 fontCol = fixed4(0, 0, 0, 0);
+				float lum = 1 - Luminance(col);
+				if(lum < 0.125){
+					fontCol = tex2D(_Font0, i.uv2);
+				}else if(lum < 0.25){
+					fontCol = tex2D(_Font1, i.uv2);
+				}else if(lum < 0.375){
+					fontCol = tex2D(_Font2, i.uv2);
+				}else if(lum < 0.5){
+					fontCol = tex2D(_Font3, i.uv2);
+				}else if(lum < 0.625){
+					fontCol = tex2D(_Font4, i.uv2);
+				}else if(lum < 0.75){
+					fontCol = tex2D(_Font5, i.uv2);
+				}else if(lum < 0.875){
+					fontCol = tex2D(_Font6, i.uv2);
+				}else{
+					fontCol = tex2D(_Font7, i.uv2);
+				}
+
+//				if(fontCol.r > 0.8){
+//					discard;
+//				}
+
+				return fontCol;
+			}
+			ENDCG
+		}
+	}
+}
